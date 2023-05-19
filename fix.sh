b0b40682a2f0af9cc7d8ab7488c8dfdc2a2a9fe9
momiji <momiji>
2023-05-19 22:11:32 +0200

    configure workflows

--- fix.sh
+++ fix.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+set -Eeuo pipefail
+cd "$(dirname "$0")"
+
+git status -s | grep "^ M " | awk '{print $2}' | xargs -r git add
+
 date > fix
 git commit -m "fix: automatic" fix
+
 git push
+
+curl -v -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/momiji/gh-workflows/dispatches -d '{ "event_type": "manual-semantic" }'
