b970b5c9443718a5022c826e0f606c3ebcb911d2
momiji <momiji>
2023-05-19 22:12:04 +0200

    configure workflows

--- fix.sh
+++ fix.sh
@@ -2,11 +2,13 @@
 set -Eeuo pipefail
 cd "$(dirname "$0")"
 
-git status -s | grep "^ M " | awk '{print $2}' | xargs -r git add
+git status -s | sed -nr "/^(M| M)/p" | awk '{print $2}' | xargs -i git add {}
+git commit -m "configure workflows"
 
 date > fix
 git commit -m "fix: automatic" fix
 
+git pull --rebase
 git push
 
 curl -v -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/momiji/gh-workflows/dispatches -d '{ "event_type": "manual-semantic" }'
